be81a11951a399b37a79828c23226f8248cc1993
liangbx <liangbx361@163.com>
2016-01-02 22:34:30 +0800

    初始配置

---
+++ mvvm/src/main/java/com/liangbx/android/practice/App.java
@@ -0,0 +1,20 @@
+package com.liangbx.android.practice;
+
+import android.app.Application;
+
+/**
+ * Author liangbx
+ * Date 2016/1/2
+ */
+public class App extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
---
+++ mvvm/src/main/java/com/liangbx/android/practice/view/SplashActivity.java
@@ -0,0 +1,11 @@
+package com.liangbx.android.practice.view;
+
+import android.app.Activity;
+
+/**
+ * Author liangbx
+ * Date 2016/1/2
+ */
+public class SplashActivity extends Activity {
+
+}
